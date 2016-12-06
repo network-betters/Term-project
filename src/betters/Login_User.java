502f014fc5aba59e55aa95bd87ca9b693e297b7d
wonsangL <harryandws@gmail.com>
2016-12-06 21:36:11 +0900

    before merge

--- src/betters/Login_User.java
+++ src/betters/Login_User.java
@@ -2,23 +2,18 @@ package betters;
 
 import java.awt.Color;
 import java.awt.Font;
-import java.awt.Toolkit;
+import java.awt.Graphics;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.io.PrintWriter;
 
-import javax.swing.GroupLayout;
-import javax.swing.GroupLayout.Alignment;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
-import javax.swing.LayoutStyle.ComponentPlacement;
 import javax.swing.border.EmptyBorder;
 
 
@@ -27,27 +22,37 @@ public class Login_User extends JFrame {
 	private JPanel LoginPane;
 	private JTextField EnterName;
 	private JButton btnSubmit;
+	private JLabel back;
+	ImageIcon bg;
 	public String Enter_UserName = "";
 	
 	public Login_User(PrintWriter pt){
 		this.out = pt;
+		bg = new ImageIcon("./src/image/LOGIN.png");
 		
-		setIconImage(Toolkit.getDefaultToolkit()
-				.getImage(Login.class.getResource("/com/sun/javafx/scene/web/skin/Bold_16x16_JFX.png")));
 		setTitle("Login");
-		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 294, 180);
-		LoginPane = new JPanel();
-		LoginPane.setBackground(Color.WHITE);
+		LoginPane = new JPanel(){
+		      public void paintComponent(Graphics g){
+		             g.drawImage(bg.getImage(), 0, 0, null);
+		             setOpaque(false);
+		             super.paintComponent(g);
+		       }
+		       };;
 		LoginPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(LoginPane);
+		LoginPane.setLayout(null);
 
 		JLabel lblEnterYourNickname = new JLabel("Enter Your Nickname");
 		lblEnterYourNickname.setFont(new Font("Bodoni MT", Font.BOLD, 19));
+		LoginPane.add(lblEnterYourNickname);
 
 		EnterName = new JTextField();
-		EnterName.setBackground(Color.LIGHT_GRAY);
+		EnterName.setBackground(Color.WHITE);
 		EnterName.setColumns(10);
+		EnterName.setBounds(49, 54, 178, 24);
+		LoginPane.add(EnterName);
  
 		btnSubmit = new JButton("Submit");
 		btnSubmit.addActionListener(new ActionListener() {
@@ -58,20 +63,9 @@ public class Login_User extends JFrame {
 					out.println(EnterName.getText());
 			}
 		});
-		GroupLayout gl_LoginPane = new GroupLayout(LoginPane);
-		gl_LoginPane.setHorizontalGroup(gl_LoginPane.createParallelGroup(Alignment.LEADING).addGroup(gl_LoginPane
-				.createSequentialGroup().addGap(49)
-				.addGroup(gl_LoginPane.createParallelGroup(Alignment.LEADING).addComponent(EnterName).addComponent(
-						lblEnterYourNickname, GroupLayout.PREFERRED_SIZE, 169, GroupLayout.PREFERRED_SIZE))
-				.addContainerGap(48, Short.MAX_VALUE))
-				.addGroup(gl_LoginPane.createSequentialGroup().addGap(92).addComponent(btnSubmit).addContainerGap(97,
-						Short.MAX_VALUE)));
-		gl_LoginPane.setVerticalGroup(gl_LoginPane.createParallelGroup(Alignment.LEADING)
-				.addGroup(gl_LoginPane.createSequentialGroup().addGap(27).addComponent(lblEnterYourNickname).addGap(2)
-						.addComponent(EnterName, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE,
-								GroupLayout.PREFERRED_SIZE)
-						.addPreferredGap(ComponentPlacement.UNRELATED).addComponent(btnSubmit)
-						.addContainerGap(GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)));
-		LoginPane.setLayout(gl_LoginPane);
+		btnSubmit.setBounds(83, 90, 120, 27);
+		btnSubmit.setIcon(new ImageIcon("./src/image/submit.png"));
+		btnSubmit.setRolloverIcon(new ImageIcon("./src/image/submitp.png"));
+		LoginPane.add(btnSubmit);
 	}
 }
--- src/betters/Quiz.java
+++ src/betters/Quiz.java
@@ -2,10 +2,10 @@ package betters;
 
 import java.awt.Color;
 import java.awt.Font;
+import java.awt.Graphics;
 import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.io.BufferedReader;
 import java.io.PrintWriter;
 
 import javax.swing.ImageIcon;
@@ -13,6 +13,7 @@ import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
 import javax.swing.border.EmptyBorder;
@@ -23,40 +24,50 @@ public class Quiz extends JFrame {
 	JTextArea ChattingArea;
 	JLabel lblMaximumRaise;
 	JLabel lblCumulative;
-	JLabel lblPoint;
+	JLabel lblminR;
+	JScrollPane scroll;
 	private JPanel QuizPane;
 	private JTextField ChattingUser;
 	private PrintWriter out;
+	private ImageIcon icon;
 	
 	public void showProblem(String problem) {
 		ProblemArea.setText(problem);
 	}
 
 	public void setLbl(DataBox db) {
-		lblMaximumRaise.setText("Maximum Raise point : " + db.maxP);;
-		lblCumulative.setText("Cumulative Point : " + db.cumulativeP);;
-		lblPoint.setText("Minimum Rase Rate : " + db.minRR);;
+		lblMaximumRaise.setText("" + db.maxP);;
+		lblCumulative.setText("" + db.cumulativeP);;
+		lblminR.setText("" + db.minRR);;
 	}
 	
 	public void chat(String msg) {
 		ChattingArea.append(msg + "\n");
+		ChattingArea.setCaretPosition(ChattingArea.getDocument().getLength());
 	}
 
 	public Quiz(PrintWriter pw) {
 		this.out = pw;
+		icon = new ImageIcon("./src//image//Quiz.png");
 
 		setIconImage(Toolkit.getDefaultToolkit()
 				.getImage(Quiz.class.getResource("/com/sun/javafx/scene/web/skin/Bold_16x16_JFX.png")));
 		setTitle("Quiz");
 		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		setBounds(100, 100, 629, 447);
-		QuizPane = new JPanel();
+		setBounds(100, 100, 629, 493);
+		QuizPane = new JPanel(){
+		      public void paintComponent(Graphics g){
+		            g.drawImage(icon.getImage(), 0, 0, null);
+		            setOpaque(false);
+		            super.paintComponent(g);
+		      }
+		};
 		QuizPane.setBackground(new Color(12, 42, 53));
 		QuizPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(QuizPane);
 
 		ChattingUser = new JTextField();
-		ChattingUser.setBounds(14, 364, 449, 24);
+		ChattingUser.setBounds(14, 403, 449, 24);
 		ChattingUser.setColumns(10);
 		ChattingUser.addActionListener(new ActionListener() {
 			@Override
@@ -81,11 +92,11 @@ public class Quiz extends JFrame {
 				}
 			}
 		});
-		btnReady.setBounds(477, 287, 138, 47);
+		btnReady.setBounds(477, 330, 116, 42);
 		btnReady.setBackground(new Color(23, 85, 110));
 
 		JButton btnExit = new JButton("Exit");
-		btnExit.setBounds(476, 346, 116, 42);
+		btnExit.setBounds(476, 385, 116, 42);
 		btnExit.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				out.println("EXIT");
@@ -98,14 +109,19 @@ public class Quiz extends JFrame {
 		ProblemArea.setEditable(false);
 		ProblemArea.setForeground(new Color(255, 255, 240));
 		ProblemArea.setBackground(new Color(23, 85, 110));
-		ProblemArea.setBounds(14, 39, 449, 145);
+		ProblemArea.setBounds(14, 83, 449, 145);
 
 		ChattingArea = new JTextArea();
-		ChattingArea.setBounds(14, 196, 449, 156);
+		ChattingArea.setBounds(14, 240, 449, 156);
+		scroll = new JScrollPane(ChattingArea);
+		scroll.setOpaque(false);
+		scroll.setBounds(14, 240, 449, 156);
+		scroll.setViewportView(ChattingArea);
+		QuizPane.add(scroll);
 		
 		QuizPane.setLayout(null);
 		QuizPane.add(ChattingUser);
-		QuizPane.add(ChattingArea);
+		//QuizPane.add(ChattingArea);
 		QuizPane.add(ProblemArea);
 		QuizPane.add(btnReady);
 		QuizPane.add(btnExit);
@@ -114,25 +130,25 @@ public class Quiz extends JFrame {
 		UserList.setEditable(false);
 		UserList.setForeground(new Color(255, 255, 240));
 		UserList.setBackground(new Color(23, 85, 110));
-		UserList.setBounds(477, 71, 116, 236);
+		UserList.setBounds(477, 80, 116, 236);
 		QuizPane.add(UserList);
 
-		lblMaximumRaise = new JLabel("Maximum Raise Rate : ");
-		lblMaximumRaise.setFont(new Font("Kristen ITC", Font.BOLD, 15));
+		lblMaximumRaise = new JLabel("");
+		lblMaximumRaise.setFont(new Font("Bauhaus 93", Font.PLAIN, 18));
 		lblMaximumRaise.setForeground(new Color(255, 255, 240));
-		lblMaximumRaise.setBounds(14, 12, 265, 18);
+		lblMaximumRaise.setBounds(188, 28, 265, 18);
 		QuizPane.add(lblMaximumRaise);
 
-		lblCumulative = new JLabel("Cumulative Point : ");
-		lblCumulative.setFont(new Font("Kristen ITC", Font.BOLD, 15));
+		lblCumulative = new JLabel("");
+		lblCumulative.setFont(new Font("Bauhaus 93", Font.PLAIN, 18));
 		lblCumulative.setForeground(new Color(255, 255, 240));
-		lblCumulative.setBounds(318, 12, 275, 18);
+		lblCumulative.setBounds(468, 28, 275, 18);
 		QuizPane.add(lblCumulative);
 
-		lblPoint = new JLabel("Minimum Rase Rate : " );
-		lblPoint.setFont(new Font("Kristen ITC", Font.BOLD, 15));
-		lblPoint.setForeground(Color.WHITE);
-		lblPoint.setBounds(477, 41, 115, 18);
-		QuizPane.add(lblPoint);
+		lblminR = new JLabel("");
+		lblminR.setFont(new Font("Bauhaus 93", Font.PLAIN, 18));
+		lblminR.setForeground(Color.WHITE);
+		lblminR.setBounds(188, 56, 115, 18);
+		QuizPane.add(lblminR);
 	}
 }
